6f485db906efddd727b1f8eb7c549a1577d35221
Abdi-01 <abdialghi@gmail.com>
2024-05-29 14:26:26 +0700

    fix: deploy run

--- deploy.config.js
+++ deploy.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'JCWD-TRY-4004',
-      script: 'npm run',
-      args: 'serve',
+      script: 'npm',
+      args: 'run serve',
       time: true,
     },
   ],
